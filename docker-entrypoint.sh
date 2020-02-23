82662ac9038d9a00139f839e74d0b496ba73c765
mydemorepo <adamvasyliuta@gmail.com>
2020-02-23 12:12:26 +0200

    Added Git

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,40 +2,37 @@
 set -euo pipefail
 
 apache_tomcat_initilizing() {
-       
-        sh /opt/tomcat/bin/startup.sh
-        echo "Apache Tomcat server initialiazed!"
+      if [[ ! -d "/opt/tomcat/webapps/app1" ]]; then
+               git config --global user.name "mydemorepo"
+               git config --global user.email adamvasyliuta@gmail.com
+               cd /opt/tomcat/webapps
+               git clone https://github.com/mydemorepo/app1.git
+               cd app1
+               git remote add app1 https://github.com/mydemorepo/app1.git
+               sh /opt/tomcat/bin/startup.sh
+      else
+               git pull app1 master
+               sh /opt/tomcat/bin/startup.sh
+      fi
 }
 
 mysql_initilizing() {
-     #MySQL service start
-        service mysql start
-     #Change the authentication method for root
-        mysql -u root -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
-     #database creation
-        cd /tmp
-        curl -LJO https://raw.githubusercontent.com/mydemorepo/files/master/sampledatabase.sql
-        mysql -uroot -proot < sampledatabase.sql;
-     #selection of the database used in the application
-        mysql -u user1 -ppass1 -e "USE classicmodels;"
-        echo "MySQL database initialized!"
+      if [[ ! -d "/var/lib/mysql/classicmodels" ]]; then
+            #MySQL service start
+               service mysql start
+            #Change the authentication method for root
+               mysql -u root -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
+            #database creation
+               mysql -uroot -proot < /opt/tomcat/webapps/app1/sampledatabase.sql;
+      else
+            #MySQL service start
+               service mysql start
+      fi
 }
 
 if [[ "$1" == /bin/bash ]]; then
-        echo ""
-        echo "***APACHE TOMCAT SERVER INITIALIZING ... ***"
-        echo ""
-        apache_tomcat_initilizing
-        echo ""
-        echo "*** APACHE TOMCAT SERVER STARTED SUCCESSFULY ***"
-        echo ""
-        echo ""
-        echo "***MySQL SERVER INITIALIZING ... ***"
-        echo ""
-        mysql_initilizing
-        echo ""
-        echo "***MySQL SERVER STARTED SUCCESSFULY ***"
-        echo ""
+         apache_tomcat_initilizing
+         mysql_initilizing
 fi
 
 
