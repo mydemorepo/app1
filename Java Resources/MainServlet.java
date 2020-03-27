d6672900e200764e62fd43715ebcc46f836fa4dd
mydemorepo <adamvasyliuta@gmail.com>
2020-03-27 15:08:01 +0200

    Added main servlet

---
+++ Java Resources/MainServlet.java
@@ -0,0 +1,23 @@
+
+import java.io.IOException;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MainServlet extends HttpServlet {
+
+	String path;
+
+	public void init() throws ServletException {
+		path = "/jsps/main.jsp";
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletContext servletContext = getServletContext();
+		RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
+		requestDispatcher.forward(request, response);
+
+	}
+
+}
