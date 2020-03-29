b8cda6d4f77d469bfb0f2661dab98e455494fecc
mydemorepo <adamvasyliuta@gmail.com>
2020-03-29 14:49:08 +0300

    Added HttpSession in MainServlet

--- Java Resources/MainServlet.java
+++ Java Resources/MainServlet.java
@@ -13,6 +13,8 @@ public class MainServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession (true);
 
 		ServletContext servletContext = getServletContext();
 		RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(path);
