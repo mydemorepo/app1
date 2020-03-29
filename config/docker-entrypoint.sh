1ebf745be712583bfb052383936b7ab0ac976a35
mydemorepo <adamvasyliuta@gmail.com>
2020-03-29 13:39:26 +0300

    git checkout docker-entrypoint.sh

--- config/docker-entrypoint.sh
+++ config/docker-entrypoint.sh
@@ -11,8 +11,6 @@ apache_tomcat_initilizing() {
          git remote add app1 https://github.com/mydemorepo/app1.git
          cp -f /opt/tomcat/webapps/app1/config/tomcatconfig/tomcatusers.xml /opt/tomcat/conf/tomcat-users.xml
          cp -f /opt/tomcat/webapps/app1/config/tomcatconfig/context_.xml /opt/tomcat/webapps/manager/META-INF/context.xml
-         cp -f /opt/tomcat/webapps/app1/config/tomcatconfig/context.xml /opt/tomcat/conf
-         cp -f /opt/tomcat/webapps/app1/config/tomcatconfig/mysql-connector-java-8.0.19.jar /opt/tomcat/lib 
          chown -R mysql:mysql /usr/lib/jvm
          chown -R mysql:mysql /opt/tomcat
          gosu mysql sh /opt/tomcat/bin/startup.sh
@@ -32,7 +30,6 @@ mysql_initilizing() {
          mysql -u root -proot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'; FLUSH PRIVILEGES;"
       #Database creation
          mysql -uroot -proot < /opt/tomcat/webapps/app1/config/mysqlconfig/sampledatabase.sql;
-         mysql -uroot -proot < /opt/tomcat/webapps/app1/config/mysqlconfig/sessions.sql;
    else
       #MySQL service start
          service mysql start
@@ -41,14 +38,14 @@ mysql_initilizing() {
 
 apache_ant_initilizing() {
    if [[ -O "/opt/ant" && -G "/opt/ant" ]]; then
-         cp /opt/tomcat/lib/catalina-ant.jar /opt/ant/lib
+         cp /opt/tomcat/lib/catalina-ant.jar /opt/ant/lib/catalina-ant.jar
          chown -R mysql:mysql /opt/ant
    fi
 }
 
 apache_http_initilizing() {
    if [[ -O "/var/www/html" && -G "/var/www/html" ]]; then
-         cp /opt/tomcat/webapps/app1/config/apache2config/000-default.conf /etc/apache2/sites-available
+         cp /opt/tomcat/webapps/app1/config/apache2config/000-default.conf /etc/apache2/sites-available/000-default.conf
          chown -R www-data:www-data /var/www/html
          a2enmod proxy
          a2enmod proxy_http
